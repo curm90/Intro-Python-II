3d449f0cf13d66d511dec732d1493862c58e061e
Liam Sutton <devliam90@gmail.com>
2020-02-12 12:10:32 +0000

    basic room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f'Room: {self.name}\n{self.description}'
+
+    def __repr__(self):
+        return f'Room({self.name}, {self.description})'
+
+
+room_1 = Room('Testing', 'This room is for testing purposes')
+print(room_1)
+print(repr(room_1))
